90a5f4e5404bda68e5e74d4714f2153c5cd77e52
Łukasz Mitka <luk.mitka@uj.edu.pl>
2015-08-28 22:56:45 +0200

    Add header file with paths to gpio's

---
+++ wandboard_hd44780_gpio.h
@@ -0,0 +1,15 @@
+// Register Select -- pin wyboru rejestu
+#define RS_PIN_DIR 		"/sys/class/gpio/gpio75/direction"
+#define RS_PIN_VALUE 	"/sys/class/gpio/gpio75/value"
+// Enable -- pin wywołania akcji
+#define E_PIN_DIR		"/sys/class/gpio/gpio91/direction"
+#define E_PIN_VALUE		"/sys/class/gpio/gpio91/value"
+// piny danych 
+#define DB4_PIN_DIR		"/sys/class/gpio/gpio24/direction"
+#define DB4_PIN_VALUE	"/sys/class/gpio/gpio24/value"
+#define DB5_PIN_DIR		"/sys/class/gpio/gpio200/direction"
+#define DB5_PIN_VALUE	"/sys/class/gpio/gpio200/value"
+#define DB6_PIN_DIR		"/sys/class/gpio/gpio90/direction"
+#define DB6_PIN_VALUE	"/sys/class/gpio/gpio90/value"
+#define DB7_PIN_DIR		"/sys/class/gpio/gpio72/direction"
+#define DB7_PIN_VALUE	"/sys/class/gpio/gpio72/value"
